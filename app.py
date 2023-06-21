35cb3cb73d2d3608d425bca0324f206eb82282e6
YutaShibuyaCoder <125983170+YutaShibuyaCoder@users.noreply.github.com>
2023-06-21 12:53:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def communicate():
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=messages
     )  
 
