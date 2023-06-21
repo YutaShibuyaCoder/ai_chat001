a51ef67aedab0fd441b38eadb45a43fb465ac9b7
YutaShibuyaCoder <125983170+YutaShibuyaCoder@users.noreply.github.com>
2023-06-21 14:43:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import openai
 
@@ -5,29 +6,34 @@ import openai
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 
-# st.session_stateを使いメッセージのやりとりを保存
+# st.session_stateを使いメッセージのやりとりと使用回数を保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
         ]
+if "count" not in st.session_state:
+    st.session_state["count"] = 0  # 初期化
 
 # チャットボットとやりとりする関数
 def communicate():
-    messages = st.session_state["messages"]
+    if st.session_state["count"] < 5:  # 使用回数が5以下の時のみ通信
+        messages = st.session_state["messages"]
 
-    user_message = {"role": "user", "content": st.session_state["user_input"]}
-    messages.append(user_message)
+        user_message = {"role": "user", "content": st.session_state["user_input"]}
+        messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )  
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )  
 
-    bot_message = response["choices"][0]["message"]
-    messages.append(bot_message)
+        bot_message = response["choices"][0]["message"]
+        messages.append(bot_message)
 
-    st.session_state["user_input"] = ""  # 入力欄を消去
-
+        st.session_state["user_input"] = ""  # 入力欄を消去
+        st.session_state["count"] += 1  # 使用回数を増やす
+    else:
+        st.warning("チャットの使用回数が上限の5回に達しました。")
 
 # ユーザーインターフェイスの構築
 st.title("My AI Assistant")
